16707ac08a3e220892e78b390cf04047df7862cf
Dev-Shankar-Paul <paulds2001@gmail.com>
2020-03-24 02:48:20 +0530

    can make slots for all days

--- D_D_M/Extract_excel.py
+++ D_D_M/Extract_excel.py
@@ -1,5 +1,6 @@
 import os
 import random
+
 try:
     import xlrd as xlr
 except ImportError:
@@ -14,7 +15,7 @@ day={}
 
 c=0
 
-while(c!=1):
+while(c!=5):
     sheet=wb.sheet_by_index(c)
     for j in range(1,sheet.ncols):
         names.clear()
@@ -33,23 +34,29 @@ while(c!=1):
 
     for i in day.keys():
         name.clear()
-        while(len(name)!=3):
-            value1=random.randrange(0,len(day[i])-1,1)
-            names=day[i]
-            name.append(names[value1])
-            day[i].remove(names[value1])
-        tt[i]=name[0:len(name)]
+        if(len(day[i])>3):
+            while(len(name)!=3):
+                value1=random.randrange(0,len(day[i])-1)
+                names=day[i]
+                name.append(names[value1])
+                day[i].remove(names[value1])
+            tt[i]=name[0:len(name)]
+        else:
+            tt[i]=day[i][1]
 
-    print(tt)
-    print('\n')
+        print(i,':',tt[i])
+    #print(tt[i])
+    #print('\n')
     for i in day.keys():
         if(len(day[i])>=3):
             sjt[i]=day[i][:3]
         else:
             sjt[i]=day[i]
-
-    print(sjt)
-
+        print(i,':',sjt[i])
+    #print(sjt)
+    #print("\n\n")
+    print('\n')
+    
     c+=1
     
 
