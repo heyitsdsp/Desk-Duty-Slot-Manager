a7f41c0d3c1a4a651e0e8f39d850661252f76838
Dev-Shankar-Paul <paulds2001@gmail.com>
2020-03-26 03:43:51 +0530

    Enabled Output in excel sheet

--- D_D_M/Extract_excel.py
+++ D_D_M/Extract_excel.py
@@ -7,15 +7,30 @@ except ImportError:
     print("This file needs XLRD to run , please execute 'pip install xlrd' and run this file again")
     exit()
 
+try:
+    import openpyxl
+except ImportError:
+    print("This file needs Openpyxl to run , please execute 'pip install openpyxl' and run this file again")
+    exit()
+
+def list_to_string(list1):
+    str1=''
+    return(str1.join(list1))
+
 loc=os.getcwd()+"\\slots.xlsx"
 
 wb=xlr.open_workbook(loc)
+new_wb=openpyxl.load_workbook('Template.xlsx')
+sheet_names=new_wb.get_sheet_names()
 names=[]
 day={}
 
 c=0
 
 while(c!=5):
+    new_sheet=new_wb.get_sheet_by_name(sheet_names[c])
+    rval=cval=1
+
     sheet=wb.sheet_by_index(c)
     for j in range(1,sheet.ncols):
         names.clear()
@@ -27,12 +42,20 @@ while(c!=5):
     sjt={}
 
     for i in day.keys():
+        rval=1
         tt[i]=[]
         sjt[i]=[]
+        new_sheet.cell(row=rval,column=cval).value=i
+        rval+=5
+        new_sheet.cell(row=rval,column=cval).value=i
+        cval+=1
 
     name=[]
-
+ 
+    cval=1
     for i in day.keys():
+        rval=2
+        x=0
         name.clear()
         if(len(day[i])>3):
             while(len(name)!=3):
@@ -44,19 +67,33 @@ while(c!=5):
         else:
             tt[i]=day[i][1]
 
-        print(i,':',tt[i])
+        #print(i,':',tt[i])
+        while(x!=len(tt[i])):
+            new_sheet.cell(row=rval,column=cval).value=tt[i][x]
+            x+=1
+            rval+=1
+        cval+=1
     #print(tt[i])
     #print('\n')
+
+    cval=1
     for i in day.keys():
+        rval=7
+        x=0
         if(len(day[i])>=3):
             sjt[i]=day[i][:3]
         else:
             sjt[i]=day[i]
-        print(i,':',sjt[i])
+        #print(i,':',sjt[i])
+        while(x!=len(sjt[i])):
+            new_sheet.cell(row=rval,column=cval).value=sjt[i][x]
+            x+=1
+            rval+=1
+        cval+=1
     #print(sjt)
     #print("\n\n")
-    print('\n')
+    #print('\n')
     
     c+=1
-    
 
+new_wb.save('test.xlsx')
